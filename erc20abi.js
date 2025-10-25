66bd14e98f12563a99382cc273b5520a7c92976f
antonfisher1995-glitch <antonfisher1995@gmail.com>
2025-10-25 15:24:27 -0700

    Create erc20abi.js

---
+++ erc20abi.js
@@ -0,0 +1,5 @@
+window.ERC20_ABI = [
+ {"constant":true,"inputs":[],"name":"symbol","outputs":[{"type":"string"}],"type":"function"},
+ {"constant":true,"inputs":[],"name":"decimals","outputs":[{"type":"uint8"}],"type":"function"},
+ {"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"type":"function"}
+];
